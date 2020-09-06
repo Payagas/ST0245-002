8d1e6532bbcf0a83dc33a09e647d87b5fe812696
pmayavi <68768011+pmayavi@users.noreply.github.com>
2020-09-06 13:26:07 -0500

    Update MergeSort.java

--- laboratorios/lab02/codigo/MergeSort.java
+++ laboratorios/lab02/codigo/MergeSort.java
@@ -1,12 +1,38 @@
 
+/**
+ * This algorithm takes the principal of divide to concor
+ * It separates recursively the array and organises it by section
+ * 
+ * @autor Pablo Maya Villegas & Miguel Angel Martinez
+ */
 class MergeSort { 
-    static void sort(int arr[]) 
-    { 
+    /**
+     * This is the starter method to facilitate the first call
+     * 
+     * @param arr the array to be organised
+     */
+    static void sort(int arr[]) { 
         sort(arr,0,arr.length-1);
     } 
     
-    static void sort(int arr[], int l, int r) 
-    { 
+    /**
+     * This is the part of the algorithm that divides the current
+     * array in 2 parts, it doesn't directly tamper with the array,
+     * it keeps count of the limits to tamper only with one 
+     * section at a time.
+     * 
+     * @param arr the array being organised
+     * @param l the leftmost limit of the section of the array
+     * @param m the middle point of the section of the array
+     * @param r the rightmost limit of the section of the array
+     * 
+     *   Adapted from:
+     * Autor: Rajat Mishra
+     * Tittle: MergeSort
+     * Type: Method
+     * Publisher: www.geeksforgeeks.org/merge-sort/
+     */
+    static void sort(int arr[], int l, int r) { 
         if (l < r) { 
             int m = (l + r) / 2; 
 
@@ -17,8 +43,22 @@ class MergeSort {
         } 
     } 
 
-    static void merge(int arr[], int l, int m, int r) 
-    { 
+    /**
+     * This is the part of the algorithm that joins organised
+     * arrays to be efficient.
+     * 
+     * @param arr the array being organised
+     * @param l the leftmost limit of the section of the array
+     * @param m the middle point of the section of the array
+     * @param r the rightmost limit of the section of the array
+     * 
+     *   Adapted from:
+     * Autor: Rajat Mishra
+     * Tittle: MergeSort
+     * Type: Method
+     * Publisher: www.geeksforgeeks.org/merge-sort/
+     */
+    static void merge(int arr[], int l, int m, int r) { 
         int n1 = m - l + 1; 
         int n2 = r - m; 
         
@@ -53,6 +93,12 @@ class MergeSort {
         } 
     } 
     
+    /**
+     * This method tests how much time the algorithm takes
+     * in the worst case scenario
+     * 
+     * @param numeroDatos the size of the array
+     */
     static void timeTester(int numeroDatos){
         int[] a = worseCase(numeroDatos);
         System.out.println(numeroDatos);
@@ -62,6 +108,14 @@ class MergeSort {
         System.out.println("Tiempo transcurrido:"+ (tf - ti));
     }
     
+    /**
+     * This method automates the creacion of time testers
+     * so that they can be easily compared.
+     * 
+     * @param numeroDatos the size of the starter array
+     * @param aumento the increase that the size goes through each cycle
+     * @param veces the amount of time testers called
+     */
     static void autoTester(int numeroDatos, int aumento, int veces){
         int lim = numeroDatos + aumento*veces;
         while(numeroDatos <= lim){
@@ -70,6 +124,13 @@ class MergeSort {
         }
     }
     
+    /**
+     * This method creates an array organised from high to low
+     * to compare the algorithms in their worst case scenario.
+     * 
+     * @param numeroDatos the size of the array
+     * @return wc the worse case array 
+     */
     static int[] worseCase(int numeroDatos){
         int[] wc = new int[numeroDatos];
         for(int i=0; i<numeroDatos; i++){
