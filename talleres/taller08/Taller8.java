47a2cca342f0190b8977d91c9785cc118cdcc320
migueflorez10 <68928440+migueflorez10@users.noreply.github.com>
2020-09-20 19:48:49 -0500

    Add files via upload

---
+++ talleres/taller08/Taller8.java
@@ -0,0 +1,47 @@
+//package Taller__8;
+import java.util.*;
+
+/**
+ * La clase Taller8 tiene la intención de
+ * enfatizar en el uso de pilas y colas ya 
+ * implementadas en java
+ * @see <a href="https://docs.oracle.com/javase/7/docs/api/java/util/Queue.html"> Ver más acerca de colas<a/>
+ * @se e <a href="https://docs.oracle.com/javase/7/docs/api/java/util/Stack.html"> Ver más acerca de pilas <a/> 
+ * @author Mauricio Toro, Andrés Páez, Daniel Mesa 
+ */
+
+ //Nota: Hacen falta algunas partes de las lineas de codigo, estas estan indicadas con una linea, ¡debes completarlas!
+public class Taller8 {
+    
+    /**
+    * @param string es una cadena de texto que viene de este modo 3 4 5 * + o de esta manera 2 3 * 5 +
+    * todo viene separado por espacios por lo que la funcion split.("") vendría muy bien para separarlos
+    * Se intenta hacer el calculo por medio del metodo polaco posfijo
+    * @return un entero resultado de la operación
+    */
+    public static int polaca  (String string){
+        String[] prefixStr = string.split(" "); 
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int i=0;i<prefixStr.length;i++){
+            if(prefixStr[i].equals("+")){
+                stack.push(stack.pop()+ stack.pop());
+            }
+            else if(prefixStr[i].equals("*")){
+                stack.push(stack.pop()*stack.pop());
+            }
+            else if(prefixStr[i].equals("-")){
+                stack.push( stack.pop() -stack.pop());
+            }
+            else if(prefixStr[i].equals("/")){
+                stack.push(stack.pop()*stack.pop());
+            }
+            else{
+                stack.push(Integer.parseInt(prefixStr[i]));
+            }
+        }
+         return stack.pop();
+    }
+}
+	
+   
