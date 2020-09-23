512fae369d667271b07d67c18029632bd675f9f8
pmayavi <68768011+pmayavi@users.noreply.github.com>
2020-09-23 17:11:32 -0500

    Add files via upload

--- talleres/taller08/Taller8.java
+++ talleres/taller08/Taller8.java
@@ -1,24 +1,7 @@
-//package Taller__8;
 import java.util.*;
 
-/**
- * La clase Taller8 tiene la intención de
- * enfatizar en el uso de pilas y colas ya 
- * implementadas en java
- * @see <a href="https://docs.oracle.com/javase/7/docs/api/java/util/Queue.html"> Ver más acerca de colas<a/>
- * @se e <a href="https://docs.oracle.com/javase/7/docs/api/java/util/Stack.html"> Ver más acerca de pilas <a/> 
- * @author Mauricio Toro, Andrés Páez, Daniel Mesa 
- */
-
-//Nota: Hacen falta algunas partes de las lineas de codigo, estas estan indicadas con una linea, ¡debes completarlas!
 public class Taller8 {
 
-    /**
-     * @param string es una cadena de texto que viene de este modo 3 4 5 * + o de esta manera 2 3 * 5 +
-     * todo viene separado por espacios por lo que la funcion split.("") vendría muy bien para separarlos
-     * Se intenta hacer el calculo por medio del metodo polaco posfijo
-     * @return un entero resultado de la operación
-     */
     public static int polaca  (String string){
         String[] prefixStr = string.split(" "); 
         Stack<Integer> stack = new Stack<>();
@@ -52,10 +35,6 @@ public class Taller8 {
         return temp;
     }
 
-    /**
-     * @param neveras es una estructura de datos para representar el almacen con las neveras
-     * @param solicitudes es una estructura de datos para representar las solicitudes
-     */
     public static void asignarSolicitudes  
     (Stack<Node> neveras, Stack<Node> solicitudes){
         while(!solicitudes.empty()){
@@ -69,13 +48,6 @@ public class Taller8 {
         }
     }
 
-    /**
-     * @param queue es una cola ya implementada que se crea en el test
-     * Este método se encarga de atender a personas.
-     * Nota: Se debe imprimir en consola a quién atiende y sacarlo de la cola
-     * existe una función "pull" que hace el trabajo más fácil
-     * 
-     */
     public static void cola (Queue<String> q){
         while(!q.isEmpty()){
             System.out.println("Atendiendo a "+q.poll());
