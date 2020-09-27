5d551649c295cd40637ddd9177f63480099fd0d7
pmayavi <68768011+pmayavi@users.noreply.github.com>
2020-09-27 18:19:10 -0500

    Update tecladoRoto.java

--- laboratorios/lab03/ejercicioEnLinea/tecladoRoto.java
+++ laboratorios/lab03/ejercicioEnLinea/tecladoRoto.java
@@ -1,19 +1,25 @@
 import java.util.LinkedList;
 public class tecladoRoto{
-    //Punto 2.1
     public static void teclado(String a){
         LinkedList<Character>texto = new LinkedList<>();
+        char c = ']';
+        int j = 0;
         for(int i=0; i < a.length(); ++i){
-            if(a.charAt(i) != ']' && a.charAt(i) != '['){
+            if(a.charAt(i) == ']') c = ']';
+            else if(a.charAt(i) == '[') {
+                c = '[';
+                j = 0;
+            }else if(c == ']'){
                 texto.addLast(a.charAt(i));
+                j = 0;
+            }else if(c == '['){
+                texto.add(j,a.charAt(i));
+                j++;
             }
-
         }
         while(texto.size()>0){
             System.out.print(texto.getFirst());
             texto.remove();
-             
         }
-    
     }
 }
